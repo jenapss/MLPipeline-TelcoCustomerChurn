687dfc00ca61c3818346fc3975ec5537be67c357
Jelaleddin Sultanov <jelaleddinsultanov@yandex.ru>
2022-02-24 17:53:25 +0500

    tested the test cases

--- components/check_data/test_data.py
+++ components/check_data/test_data.py
@@ -10,7 +10,6 @@ def test_column_names(data):
     '''
 
     expected_colums = [
-        'customerID',
         'gender',
         'SeniorCitizen',
         'Partner',
@@ -34,31 +33,17 @@ def test_column_names(data):
     ]
 
     these_columns = data.columns.values
+    
+    print(these_columns,'----------')
+    print(expected_colums,'1111111')
 
     # This also enforces the same order
     assert list(expected_colums) == list(these_columns)
 
 
-def test_neighborhood_names(data):
-    '''
-    Test neighbourhood column names.
-
-    '''
-
-    known_names = ["Bronx", "Brooklyn", "Manhattan", "Queens", "Staten Island"]
-
-    neigh = set(data['neighbourhood_group'].unique())
-
-    # Unordered check
-    assert set(known_names) == set(neigh)
 
 
 ########################################################
 # Implement here test_row_count and test_price_range   #
 ########################################################
-def test_row_count(data):
-    """
-    Test the row count 
-    """
-    assert 15000 < data.shape[0] < 1000000
 
